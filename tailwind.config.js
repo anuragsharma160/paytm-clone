87cae62badfa9c57ec9476da57dde6a886552365
Anurag Sharma <blstyle90@gmail.com>
2024-10-05 21:46:23 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        skyblue: '#00BAF2',
+        darkblue: '#0F4A8A',
+        rcardhoverblackskyblue: '#08a0cf',
+        bcardhoverdarkblue: '#043b78',
+        dullwhite: '#e8e8e8',
+        finsecbg: '#F5F7FA',
+        cardskybluelightskyblue: '#66D6FF',
+        btndarkblue: '#002970',
+      },
+    },
+  },
+  plugins: [],
+}
+
